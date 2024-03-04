578ed5f465395a49dbc569a27efa73c2cd4b3dbb
shandskeptic <shandyka79@gmail.com>
2024-03-05 06:58:58 +0700

    mobile responsive

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,12 +5,12 @@ import { Result } from './Result';
 export const Layout = () => {
 	return (
 		<>
-			<div className="flex justify-center items-center gap-4 bg-primary-400 h-screen font-semibold text-white">
-				<div className="bg-primary-600 rounded-3xl w-full md:w-1/2 lg:w-1/4 sm:1/3 h-screen">
+			<div className="flex sm:flex-row flex-col justify-center items-center gap-4 bg-primary-400 font-semibold text-white">
+				<div className="bg-primary-300 rounded-3xl w-full md:w-1/2 lg:w-1/4 h-screen">
 					<Result />
 					<Number />
 				</div>
-				<div className="bg-primary-300 rounded-3xl w-full md:w-1/2 lg:w-1/5 sm:1/4 h-screen">
+				<div className="bg-primary-300 rounded-3xl w-full md:w-1/2 lg:w-1/4 h-screen">
 					<History />
 				</div>
 			</div>
